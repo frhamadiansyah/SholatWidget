7c50553425a2be8f20adf4fe07679036d1525fa5
Fandrian Rhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2021-12-01 13:31:14 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SholatWidget
+Rebuild sholat widget. check SholatWidgetV1 for old Sholat Widget Code
---
+++ SholatWidget/SholatWidgetApp.swift
@@ -0,0 +1,20 @@
+//
+//  SholatWidgetApp.swift
+//  SholatWidget
+//
+//  Created by Fandrian Rhamadiansyah on 01/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SholatWidgetApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
